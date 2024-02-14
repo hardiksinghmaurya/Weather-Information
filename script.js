a8101c0f5f6e68d446a52060e076d48b2f82d7f5
Hardik <hardikmaurya1502@gmail.com>
2024-02-15 02:45:58 +0530

    update whole app

--- script.js
+++ script.js
@@ -1,65 +1,88 @@
-import {api_key,api_url} from "./api.js"
+import { api_key, api_url } from "./api.js"
 const apikey = api_key
 const apiurl = api_url
 const input_search = document.querySelector(".search input")
+const defaultCity = input_search.value ? input_search.value : "lucknow"
 const btn = document.querySelector(".search button")
 const wIcon = document.querySelector(".w-icon")
+const loader=document.querySelector(".container")
+
 
 async function weather(cityinfo) {
     const call = await fetch(apiurl + cityinfo + `&appid=${apikey}`)
-    if (call.status == 404) {
-        document.querySelector(".error-msg p").style.display = "block"
-        document.querySelector(".feelslike").innerHTML = "--"
-        document.querySelector(".weather h2").innerHTML = "--"
-        document.querySelector(".weather h1").innerHTML = "--"
-        document.querySelector(".humidity").innerHTML = "--"
-        document.querySelector(".windspeed").innerHTML = "--"
-        document.querySelector(".visibility").innerHTML = "--"
-        document.querySelector(".sunrise").innerHTML = "--"
-        document.querySelector(".sunset").innerHTML = "--"
+    const data = await call.json()
+    if (call.status !== 404) {
+        document.querySelector(".error-msg p").style.display = "none"
+        showWeather(data)
     }
-
-
-    else {
-        if (call.status != 404) {
-            document.querySelector(".error-msg p").style.display = "none"
-        }
-        const data = await call.json()
-        // console.log(data)
-
-        document.querySelector(".feelslike").innerHTML = Math.floor(data.main.feels_like) + "°"
-        document.querySelector(".weather h1").innerHTML = Math.round(data.main.temp) + "°"
-        document.querySelector(".weather h2").innerHTML = data.name+" , "+data.sys.country
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
-        document.querySelector(".windspeed").innerHTML = Math.round(((data.wind.speed) * 18) / 5) + " km/h"
-        document.querySelector(".visibility").innerHTML = data.visibility / 1000 + " km"
-        document.querySelector(".sunrise").innerHTML = Math.floor(((data.sys.sunrise) / 1000) / 3660) % 24 + ":" + Math.floor(((data.sys.sunrise) / 1000) / 60) % 60 + " am"
-        document.querySelector(".sunset").innerHTML = Math.floor(((data.sys.sunset) / 1000) / 3660) % 24 + ":" + Math.floor(((data.sys.sunset) / 1000) / 60) % 60 + " pm"
-        if (data.weather[0].main == "Haze") {
-            wIcon.src = "images/haze.png"
-        }
-        else if (data.weather[0].main == "Clear") {
-            wIcon.src = "images/clear.png"
-        }
-        else if (data.weather[0].main == "Clouds") {
-            wIcon.src = "images/clouds.png"
-        }
-        else if (data.weather[0].main == "Drizzle") {
-            wIcon.src = "images/drizzle.webp"
-        }
-        else if (data.weather[0].main == "Mist") {
-            wIcon.src = "images/mist.png"
-        }
-        else if (data.weather[0].main == "Rain") {
-            wIcon.src = "images/rain.png"
-        }
-        else if (data.weather[0].main == "Snow") {
-            wIcon.src = "images/snow.webp"
-        }
-    }
-
+    else error()
 }
 
+weather(defaultCity)
+
+
+function showWeather(data){
+    loader.style.display="none"
+    
+    document.querySelector(".feelslike").innerHTML = Math.floor(data.main.feels_like) + "°"
+    document.querySelector(".weather h1").innerHTML = Math.round(data.main.temp) + "°"
+    document.querySelector(".weather h2").innerHTML = data.name + " , " + data.sys.country
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
+    document.querySelector(".windspeed").innerHTML = Math.round(((data.wind.speed) * 18) / 5) + " km/h"
+    document.querySelector(".visibility").innerHTML = data.visibility / 1000 + " km"
+    
+    const timezone = 'Asia/Kolkata';
+    // sunrise
+    let sunrise = data.sys.sunrise
+    let sunriseDate = new Date(sunrise * 1000)
+    const sunriseTime = sunriseDate.toLocaleString('en-IN', { timeZone: timezone, hour: 'numeric', minute: 'numeric', hour12: true });
+    document.querySelector(".sunrise").innerHTML = sunriseTime
+
+    // sunset
+    let sunset = data.sys.sunset
+    let sunsetDate = new Date(sunset * 1000)
+    const sunsetTime = sunsetDate.toLocaleString('en-IN', { timeZone: timezone, hour: 'numeric', minute: 'numeric', hour12: true });
+    document.querySelector(".sunset").innerHTML = sunsetTime
+
+    switch (data.weather[0].main) {
+        case "Haze": wIcon.src = "images/clear.png"
+            break
+        case "Clear": wIcon.src = "images/clouds.png"
+            break
+        case "Clouds": wIcon.src = "images/clouds.png"
+            break
+        case "Drizzle": wIcon.src = "images/drizzle.webp"
+            break
+        case "Mist": wIcon.src = "images/mist.png"
+            break
+        case "Rain": wIcon.src = "images/rain.png"
+            break
+        case "Snow": wIcon.src = "images/snow.webp"
+            break
+    }
+}
+
+function error() {
+    document.querySelector(".error-msg p").style.display = "block"
+    document.querySelector(".feelslike").innerHTML = "--"
+    document.querySelector(".weather h2").innerHTML = "--"
+    document.querySelector(".weather h1").innerHTML = "--"
+    document.querySelector(".humidity").innerHTML = "--"
+    document.querySelector(".windspeed").innerHTML = "--"
+    document.querySelector(".visibility").innerHTML = "--"
+    document.querySelector(".sunrise").innerHTML = "--"
+    document.querySelector(".sunset").innerHTML = "--"
+}
+
+
 btn.addEventListener("click", function () {
+    loader.style.display="block"
     weather(input_search.value)
 })
+
+
+
+
+
+
+
